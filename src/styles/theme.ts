069ac1527bd78e411b378bcfc2936fbc71e1abcf
Manuel Ricardo Nvula <177589704+Manuelema3@users.noreply.github.com>
2026-03-31 01:05:20 -0300

    Aprimoramento da estrutura base com criação das pasta images, icons e fonts

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,7 +3,7 @@ export const THEME = {
     BACKGROUND: '#F2F2F7',
     PRIMARY: '#007AFF',    // Um azul padrão, você pode mudar depois!
     SUCCESS: '#34C759',
-    DANGER: '#FF3B30',
+    DANGER: '#e53434',
     TEXT: '#1C1C1E',
     GRAY: '#8E8E93',
     WHITE: '#FFFFFF',
